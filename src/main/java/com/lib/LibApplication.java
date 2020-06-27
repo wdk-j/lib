f6c09084cbee6eff69cb82cc12a3dd1dc5a3913e
王大可 <374461948@qq.com>
2020-06-27 22:06:31 +0800

    图书管理系统demo

--- src/main/java/com/lib/LibApplication.java
+++ src/main/java/com/lib/LibApplication.java
@@ -1,8 +1,9 @@
 package com.lib;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan(value = "com.lib.mapper")
 @SpringBootApplication
 public class LibApplication {
 
--- src/test/java/com/lib/LibApplicationTests.java
+++ src/test/java/com/lib/LibApplicationTests.java
@@ -1,13 +1,22 @@
 package com.lib;
 
+import com.lib.bean.Book;
+import com.lib.mapper.BookMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class LibApplicationTests {
 
+    @Autowired
+    private BookMapper bookMapper;
     @Test
     void contextLoads() {
+        List<Book> list = bookMapper.list();
+        System.out.println(list);
     }
 
 }
